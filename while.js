3bd117fce2024d74f8879102adc4f0d741919416
asmaa <asmaa.alfauri@gmail.com>
2019-07-11 16:23:02 +0300

    finish 1-11 while ex me and Sharafaldeen

--- while.js
+++ while.js
@@ -3,14 +3,14 @@
 // Please be sure to conect this file to another HTML file
 // to seprate it from the recursion file
 
-// dDont serach on the internet except if it is written in the question
+// Dont serach on the internet except if it is written in the question
 
 // You should solve all the question by what you understand from the lecture
 // and with your pair not solo
 
-// And dont look or use a code that you wrote before 
+// And dont look or use a code that you wrote before
 
-console.log('Here is : While') //edgewjfhdgsdh
+console.log("Here is : While"); //edgewjfhdgsdh
 
 /*
 1
@@ -22,40 +22,92 @@ Ex: subtract(2); => 2 - 1 - 0 => 1
 Ex: subtract(5); => 5 - 4 - 3 - 2 - 1 - 0 => -5
 Ex: subtract(9); => 9 - 8 - 7 - 6 - 5 - 4 - 3 - 2 - 1 - 0 => -27
 */
-
-
+function subtract(n) {
+  var s = n;
+  while (n > 0) {
+    n -= 1;
+    s -= n;
+  }
+  return s;
+}
+console.log(subtract(5));
 /*
 2
 Create a function called factorial
 that takes a single parameter n,
 and return the product of all integers up to n starting from 1
-Ex: factorial(2); => 2 * 1 => 6
+Ex: factorial(2); => 2 * 1 => 2
 Ex: factorial(4); => 4 * 3 * 2 * 1 => 24
 */
 
-
+function factorial(num) {
+  var s = 1;
+  while (num > 0) {
+    s *= num;
+    num -= 1;
+  }
+  return s;
+}
+console.log(factorial(4));
 /*
 3
 Create a function called repeatStr
 that takes 2 parameters string and number,
 and return the string number time with space
-Important: the stop condition should be [somthing=== 0]
+Important: the stop condition should be [somthing!== 0]
 ** Think how we can return nothing ???
 ** Important: You should dint have extra space at the end
 Ex: repeatStr("to",2); => "to to"
 Ex: repeatStr("to",4); => "to to to to"
 */
-
-
+function repeatStr(str, num) {
+  var s = "";
+  while (num !== 0) {
+    s = s + " " + str;
+    num -= 1;
+  }
+  return s;
+}
+console.log(repeatStr("to", 5));
 /*
 4
 Create a function called sum2
 that take two parameter
-and will return the sumation from the first number to the second number
+and will return the sumation from the first number to 
+//the second number
 Ex: sum2(4, 5); => 4 + 5 => 9
 Ex: sum2(3, 6); => 3 + 4 + 5 + 6 => 18
 */
 
+function sum2(num1, num2) {
+  var x = num2;
+  while (num1 < num2) {
+    x += num1;
+    num1 += 1;
+  }
+  return x;
+}
+console.log(sum2(3, 6));
+
+// function sum2(num1, num2) {
+//     var s = num2
+//     while (num1 !== num2) {
+//         s = s + num1
+//         num1 = num1 + 1
+//     }
+//     return s
+// }
+// console.log(sum2(3, 6))
+//other solution
+// function sum2(num1, num2) {
+//     var s = num1
+//     while (num1 !== num2) {
+//         s = s + num2
+//         num2 = num2 - 1
+//     }
+//     return s
+// }
+// console.log(sum2(3, 6))
 
 /*
 5
@@ -63,59 +115,108 @@ Create a function called repeatStr2
 that takes 2 string parameters,
 and return the first string number time
 (of the lenght of the secend string) with space betweeen it
-** Important: the stop condition should be [somthing=== 0]
+** Important: the stop condition should be [somthing!== 0]
 ** Important: You should dint have extra space at the end
 Ex: repeatStr2("ro","cc"); => "ro ro"
 Ex: repeatStr2("ro","fff"); => "ro ro ro"
 */
-
-
+function repeatStr2(str1, str2) {
+  var str2Length = str2.length;
+  var x = "";
+  while (str2Length !== 0) {
+    x = x + " " + str1;
+    str2Length -= 1;
+  }
+  return x;
+}
+console.log(repeatStr2("ahmad", "asma"));
 /*
 6
 Create a function called multiOf
 that takes 3 parameters,
 and return the first number mutiple by
 (the second one ^ the third number)
-** Important: dont use **
+** Important: dont use ** (a,b,c) a*(b^c)
 Ex: multiOf(4,10,3); => 4000
 Ex: multiOf(6,3,2); => 54
 Ex: multiOf(6,2,3); => 48
 */
 
+function multiOf(num1, num2, num3) {
+  var x = 1;
+  while (num3 !== 0) {
+    x *= num2;
+    num3 -= 1;
+  }
+  return x * num1;
+}
 
+console.log(multiOf(4, 10, 0));
+console.log(multiOf(6, 3, 2));
 /*
 7
 Create a function called muti2
 that take two parameter
-and will return the multiplication from the first number to the second number
+and will return the multiplication from the first number to
+// the second number
 Ex: muti2(4, 5); => 4 * 5 => 20
 Ex: muti2(3, 6); => 3 * 4 * 5 * 6 => 360
 */
+function muti2(num1, num2) {
+  var x = 1;
+  while (num1 <= num2) {
+    x = x * num1;
+    num1 += 1;
+  }
+  return x;
+}
+console.log(muti2(3, 6));
 
-
+// function muti2(a, b) {
+//     var x=1
+//     while (b>=a) {
+//         x=x*b
+//         b=b-1
+//     }
+//     return x
+// }
+// console.log(muti2(3,6))
 /*
 8
 Create a function called numberBetweenUs
 that take 2 parameters
 and return the number between them
-** Important: You should dint have extra space at the end or comma
-** Important: the stop condition should [num1===num2-1]
+** Important: You should dint have extra space at the end 
+//or comma
+** Important: the stop condition should [num1!==num2-1]
 ** hint: maybe you will have 2 if statment
 numberBetweenUs(2,8) => "3, 4, 5, 6, 7"
 numberBetweenUs(1,3) => "2"
 */
-
+function numberBetweenUs(num1, num2) {
+  var x = "";
+  var num11 = num1 + 1;
+  var num22 = num2 - 1;
+  while (num11 !== num2 - 1) {
+    x = x + " " + num11 + ",";
+    num11 += 1;
+  }
+  return x + " " + num22;
+}
+console.log(numberBetweenUs(3, 10));
 
 /*
 9
 Write a function called countDown
 that takes one parameter
-and return a string represent the count down from the number
+and return a string represent the count down from 
+//the number
 that insert to 0
-instead of 0 need to ahve "done"
+instead of 0 you should have "done"
 between each number a comma and space
-** you need to use recursion
-** Important: You should dint have extra space at the end or comma
+** you don't need to use recursion
+** Important: You should't have extra space at 
+//the end and comma
 countDown(5)
 => "5, 4, 3, 2, 1, done"
 countDown(2)
@@ -123,8 +224,21 @@ countDown(2)
 countDown(7)
 => "7, 6, 5, 4, 3, 2, 1, done"
 */
-
+function countDown(num) {
+  var x = "";
+  while (num > 0) {
+    x = x + num + " ,";
+    num = num - 1;
+  }
+  return x + "done";
+}
+console.log(countDown(12));
+// trick in we make string in x because we want appeare
+//all of numbers counter we should convert them to string by
+//add x as string + num
 /*
+
+
 10
 Write a function called multiplication2
 that takes two parameters
@@ -134,6 +248,16 @@ multiplication2(5,4) => 20
 multiplication2(2,8) => 16
 multiplication2(7,6) =>  42
 */
+
+function multiplication2(num1, num2) {
+  var x = 0;
+  while (num1 > 0) {
+    x += num2;
+    num1 -= 1;
+  }
+  return x;
+}
+console.log(multiplication2(2, 8));
 
 /*
 11
@@ -146,6 +270,15 @@ mod2(2,8) => 2
 mod2(7,4) => 3
 */
 
+function mod2(num1, num2) {
+  var x = num1;
+  while (x >= num2) {
+    x = x - num2;
+  }
+  return x;
+}
+
+console.log(mod2(4, 4));
 
 /*
 12
@@ -162,11 +295,13 @@ repeatChar("School","s") => 1
 try more case by yourself
 */
 
-
-
-
-
-
+function repeatChar (str1,str2){
+    var x=
+    while () {
+        x=
+    }
+    return x
+}
 
 
 
@@ -175,12 +310,12 @@ try more case by yourself
 
 // Advanced Part {for your benefits}
 
-// Dont solve any question here if you didnt finish all the question a bove with your pair
+// Dont solve any question here if you didnt
+//finish all the question a bove with your pair
 
 // This part you can try it and solve solo not with your pair
 
 // This part is extra (and there is no help from the trainer) it is good to try it
-
 
 /*
 1
@@ -197,7 +332,6 @@ fibon(n): 1 1 2 3 5 8 13 21
 n:        0 1 2 3 4 5  6  7
 */
 
-
 /*
 2
 Write a function called mirror
@@ -210,8 +344,6 @@ mirror("school") => "loohcs"
 mirror("car") => "rac"
 mirror("maDrasa") => "asarDam"
 */
-
-
 
 /*
 3
@@ -226,8 +358,6 @@ mirrorCaseAlso("sChOol") => "LOoHcS"
 mirrorCaseAlso("THOR") => "roht"
 mirrorCaseAlso("BaBa") => "AbAb"
 */
-
-
 
 /*
 4
@@ -244,7 +374,6 @@ repeatChar2("school","a") => 0
 repeatChar2("School","S") => 1
 try more case by yourself
 */
-
 
 /*
 5
